ae8da82e4b24d4c252f5e6c5b2aef177dcd4d37c
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-12-09 17:01:41 -0300

    fiz alteraçao no html

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// 2. A cor de fundo de cada elemento da paleta deverá ser a cor que o elemento representa. A única cor não permitida na paleta é a cor branca.;
+// 6.  Defina a cor preta como cor inicial. Ao carregar a página, a cor preta já deve estar selecionada para pintar os pixels. O elemento da cor preta deve possuir, inicialmente, a classe selected;
+// 7. Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
+// 8. Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.
+//9. Verifica se ao clicar no botão, o quadro de pixels é totalmente preenchido de branco
+
+//começa com preto (classe selected). ao clicar em um quadro, vai adicionar uma classe. se eu clicar em um botão branco, fica na cor da classe.
+
+window.onload = function (){
+    let btnblack = document.
+}
