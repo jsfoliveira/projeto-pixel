f7c4eed2824a6f912749b3a59f4154d75e86a480
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-12-09 18:12:16 -0300

    preenchidos os requisitos de 1 - 6, parte do 9

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 // 6.  Defina a cor preta como cor inicial. Ao carregar a página, a cor preta já deve estar selecionada para pintar os pixels. O elemento da cor preta deve possuir, inicialmente, a classe selected;
+
 // 7. Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
 // 8. Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.
-//9. Verifica se ao clicar no botão, o quadro de pixels é totalmente preenchido de branco
 
 //começa com preto (classe selected). ao clicar em um quadro, vai adicionar uma classe. se eu clicar em um botão branco, fica na cor da classe.
 
-window.onload = function (){
-    let btnblack = document.
-}
+// window.onload = function () {
+//   let paleta = document.getElementById('color-palette');
+
+//   let botao = document.getElementById('clear-board');
+//   botao;
+//9. Verifica se ao clicar no botão, o quadro de pixels é totalmente preenchido de branco
+
+// botao.addEventListener('click', function () {
+//   let colunaDePixel = document.getElementById('pixel-board');
+//   for (i = 0; i < colunaDePixel.length; i += 1) {
+//     colunaDePixel.style.backgroundColor = 'white';
+//   }
+// });
+// };
