6bbfcc667e6ffc40b2385e34f0e4d16072806c34
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-12-09 22:43:52 -0300

    passei em alguns requisitos do 7 e 8, nao todo

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 // 6.  Defina a cor preta como cor inicial. Ao carregar a página, a cor preta já deve estar selecionada para pintar os pixels. O elemento da cor preta deve possuir, inicialmente, a classe selected;
 
+window.onload = function corInicial() {
+  let corPreta = document.getElementsByClassName('color')[0];
+  corPreta.classList.add('selected');
+};
+
 // 7. Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
+
+//acessa o color da paleta
+//cria array pra percorrer cada color da paleta
+//em cada color vou fazer um addEventListener para escutar o evento click e acionar a função pegar a cor
+//vou remover da lista a classe selected
+//vou pegar uma variável que vai ler o texto que está em cada .color e transformar em cor
+
+let color = document.querySelector('.color');
+color.addEventListener('click', function pegarCor(event) {
+  color.classList.remove('selected');
+  let cor = event.target.innerText;
+  return cor;
+});
+
+// let paleta = document.querySelector('#color-palette');
+// for (i = 0; i < paleta.length; i += 1) {
+//   paleta[i].addEventListener('click', function pegarCor(event) {
+//     paleta[i].classList.remove('selected');
+//     let cor = event.target.innerText;
+//     paleta[i].style.backgroundColor = cor; //falta adicionar a cor
+//   });
+// }
 // 8. Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.
 
-//começa com preto (classe selected). ao clicar em um quadro, vai adicionar uma classe. se eu clicar em um botão branco, fica na cor da classe.
-
-// window.onload = function () {
-//   let paleta = document.getElementById('color-palette');
-
-//   let botao = document.getElementById('clear-board');
-//   botao;
-//9. Verifica se ao clicar no botão, o quadro de pixels é totalmente preenchido de branco
-
-// botao.addEventListener('click', function () {
-//   let colunaDePixel = document.getElementById('pixel-board');
-//   for (i = 0; i < colunaDePixel.length; i += 1) {
-//     colunaDePixel.style.backgroundColor = 'white';
-//   }
+// let pixel = document.querySelectorAll('.pixel');
+// pixel.addEventListener('click', function mudarCor(event) {
+//   pixel.style.backgroundColor = cor;
 // });
-// };
+
+//9. Verifica se ao clicar no botão, o quadro de pixels é totalmente preenchido de branco
+let botao = document.getElementById('clear-board');
+botao.addEventListener('click', function () {
+  let colunaDePixel = document.getElementById('pixel-board');
+  colunaDePixel.style.backgroundColor = 'white';
+});
